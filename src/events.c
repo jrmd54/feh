b20fdcaf26f92a4460350133907407f4bd56d612
jdesroziers <j.desroziers@cyber-detect.eu>
2024-09-30 13:54:41 +0200

    disabled next image on click, always pan

--- src/events.c
+++ src/events.c
@@ -227,9 +227,13 @@ static void feh_event_handle_ButtonPress(XEvent * ev)
 		D(("blur starting at %d, %d\n", ev->xbutton.x, ev->xbutton.y));
 
 	} else if (feh_is_bb(EVENT_pan, button, state)) {
-		D(("Next button, but could be pan mode\n"));
-		opt.mode = MODE_NEXT;
-		winwid->mode = MODE_NEXT;
+        // Cf https://github.com/derf/feh/issues/411, never using left click to go to next image
+		// D(("Next button, but could be pan mode\n"));
+		// opt.mode = MODE_NEXT;
+		// winwid->mode = MODE_NEXT;
+ 		D(("Next button patched out; going into pan mode\n"));
+ 		opt.mode = MODE_PAN;
+ 		winwid->mode = MODE_PAN;
 		D(("click offset is %d,%d\n", ev->xbutton.x, ev->xbutton.y));
 		winwid->click_offset_x = ev->xbutton.x - winwid->im_x;
 		winwid->click_offset_y = ev->xbutton.y - winwid->im_y;
